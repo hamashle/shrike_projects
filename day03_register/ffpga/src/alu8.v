cb9bc9c51a6d47816cd940be5ae44e17834832fb
HAMADA Shoma <kamaboko_0228@icloud.com>
2026-09-14 19:38:29 +0900

    ループの実装、実行

--- day03_register/ffpga/src/alu8.v
+++ day03_register/ffpga/src/alu8.v
@@ -5,7 +5,7 @@
 module alu8 (
     input  wire [7:0] a,
     input  wire [7:0] b,
-    input  wire [1:0] op,
+    input  wire [2:0] op,
     output wire [7:0] result,
     output wire       carry_out,
     output wire       zero_flag
@@ -16,6 +16,8 @@ wire [7:0] or_result;
 wire [7:0] xor_result;
 wire [7:0] add_result;
 wire       add_carry;
+wire [7:0] sub_result;
+wire       sub_carry;
 
 assign and_result = a & b;
 assign or_result  = a | b;
@@ -30,14 +32,24 @@ adder8 adder (
     .cout(add_carry)
 );
 
+adder8 subtractor (
+    .a   (a),
+    .b   (~b),
+    .cin (1'b1),
+    .sum (sub_result),
+    .cout(sub_carry)
+);
+
 assign result =
-    (op == 2'b00) ? and_result :
-    (op == 2'b01) ? or_result :
-    (op == 2'b10) ? xor_result :
-                     add_result;
+    (op == 3'b000) ? and_result :
+    (op == 3'b001) ? or_result :
+    (op == 3'b010) ? xor_result :
+    (op == 3'b011) ? add_result :
+    (op == 3'b100) ? sub_result :
+                     8'd0;
 
 assign carry_out =
-    (op == 2'b11) ? add_carry : 1'b0;
+    (op == 3'b011) ? add_carry : 1'b0;
 assign zero_flag = (result == 8'd0);
 endmodule
 
